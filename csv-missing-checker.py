66d25005731604a25c96577f57d84801c376de5d
Grzegorz Mozer <propbono@gmail.com>
2015-10-18 16:07:29 -0400

    Add path to folders when test files will be kepts

--- csv-missing-checker.py
+++ csv-missing-checker.py
@@ -1,16 +1,32 @@
+import datetime
+
+
 __author__ = 'prepress'
 import csv, os, sys
 import timeit
 from Status import status
 
 DIR = os.path.dirname(sys.argv[0])
-PREPPED_PDF_PATH = "N:\\"
-PRESS_READY_PDF_PATH = "Q:\\"
-SOURCE_CSV_PATH = "O:\\"
-MERGED_CSV_REMOTE = "K:\\"
-PREPPED_PDF_DONE_PATH = os.path.join(PREPPED_PDF_PATH,"00Done")
-MERGED_CSV_LOCAL = os.path.join(DIR,"merged_csv")
-CSV_HEADERS = ['Upload', 'Status']
+
+NEW_UPLOADS_PATH = os.path.join(DIR,"00New")
+PREPPED_UPLOADS_PATH = os.path.join(DIR,"00Prepped")
+INAPROVAL_UPLOADS_PATH = os.path.join(DIR,"00InApproval")
+ONHOLD_UPLOADS_PATH = os.path.join(DIR,"00OnHold")
+CANCELLED_UPLOADS_PATH = os.path.join(DIR,"00Cancelled")
+PRESSREADY_UPLOADS_PATH = os.path.join(DIR,"00PressReady")
+DONE_UPLOADS_PATH = os.path.join(DIR,"00Done")
+SIGNS_UPLOADS_PATH = os.path.join(DIR,"00Signs")
+DIGITAL_UPLOADS_PATH = os.path.join(DIR,"00Digital")
+OUTSOURCED_UPLOADS_PATH = os.path.join(DIR,"00Outsourced")
+
+# PREPPED_PDF_PATH = "N:\\"
+# PRESS_READY_PDF_PATH = "Q:\\"
+# SOURCE_CSV_PATH = "O:\\"
+# MERGED_CSV_REMOTE = "K:\\"
+# PREPPED_PDF_DONE_PATH = os.path.join(PREPPED_PDF_PATH,"00Done")
+# MERGED_CSV_LOCAL = os.path.join(DIR,"merged_csv")
+
+CSV_HEADERS = ['Upload', 'Status', 'Date', 'Run']
 
 CSV_DICT = {}
 CSV_LIST = []
@@ -21,11 +37,12 @@ def save_initial_data_to_csv(name):
     print("Creating csv list:")
     csv_list_tic = timeit.default_timer()
 
-    orders_list = list_of_csv_files()
+    all_uploads_list = return_all_upload_numbers()
 
     csv_list_toc = timeit.default_timer()
     print("Pdf list - created!", "time (s): ", round(csv_list_toc - csv_list_tic, 4))
     print()
+
 
     print("Creating csv db:")
     csv_db_tic = timeit.default_timer()
@@ -33,13 +50,17 @@ def save_initial_data_to_csv(name):
     with open(name, 'a', newline='') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=CSV_HEADERS)
         writer.writeheader()
-        save_rows_to_csv(orders_list, writer, status.Done)
+        for element in all_uploads_list:
+            row = {CSV_HEADERS[0]: element, CSV_HEADERS[1]: status.New,
+                   CSV_HEADERS[2]: datetime.date.today(),CSV_HEADERS[3]: None }
+            writer.writerow(row)
 
     csv_db_toc = timeit.default_timer()
     print("CSV db - created!", "time (s): ", round(csv_db_toc - csv_db_tic, 4))
 
 
-def list_of_csv_files():
+# I think I can create dictionary here and have only one loop over the csv's
+def return_all_upload_numbers():
     orders_list = [c.split("-")[0] for c in sorted(os.listdir(SOURCE_CSV_PATH)) if
                 c.lower().endswith('.csv')]
     return orders_list
@@ -49,14 +70,10 @@ def list_of_press_ready_pdfs():
                 p.lower().endswith('.pdf') and p.lower().startswith('U') ]
     return pdf_list
 
-def save_rows_to_csv(csv_list, writer, status):
-    for element in csv_list:
-        row = {CSV_HEADERS[0]: element, CSV_HEADERS[1]: status}
-        writer.writerow(row)
 
 def update_db(name):
     db_status = ""
-    all_orders = list_of_csv_files()
+    all_orders = return_all_upload_numbers()
     orders_in_db = #grab orders from csv_db
     press_ready_pdfs = list_of_press_ready_pdfs()
 
