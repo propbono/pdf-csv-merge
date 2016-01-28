4ae3b82adff4273af0b50594245d3c3d59f51a9f
Grzegorz Mozer <propbono@gmail.com>
2016-01-28 02:45:31 -0500

    New class responsible for creating Products

---
+++ Product.py
@@ -0,0 +1,92 @@
+class Flat:
+    CSV_HEADERS_FLAT = ["comment", "Name", "Quantity", "Width", "Height",
+                        "StockVendor", "StockName", "StockWeight", "IGNORED1",
+                        "IGNORED2", "Priority", "TopOffcut", "LeftOffcut",
+                        "BottomOffcut", "RightOffcut", "ProductID",
+                        "Description", "Notes", "DueDate", "CompanyName",
+                        "FirstName", "LastName", "ContentFile", "PageColor1",
+                        "PageColor2", "ProductGroup", "Grain", "BleedsTop",
+                        "BleedsLeft", "BleedsBottom", "BleedsRight",
+                        "FolioSide1", "FolioSide2"]
+
+
+    def __init__(self, pdf, notes):
+        self.pdf = pdf
+        self.notes = notes
+        self.row = {"comment": "MetrixCSV2.0_FLAT", "Name": pdf[:-4],
+                         "Quantity": notes["quantity"],
+                         "Width": notes["width"],
+                         "Height": notes["height"], "StockVendor": "PG",
+                         "StockName": notes["stockname"],
+                         "StockWeight": notes["stockweight"], "IGNORED1": '',
+                         "IGNORED2": '', "Priority": '5', "TopOffcut": '0',
+                         "LeftOffcut": '0', "BottomOffcut": '0',
+                         "RightOffcut": '0',
+                         "ProductID": '', "Description": pdf.split('-')[0],
+                         "Notes": '', "DueDate": '',
+                         "CompanyName": pdf.split('-')[2],
+                         "FirstName": '', "LastName": '', "ContentFile": '',
+                         "PageColor1": '', "PageColor2": '',
+                         "ProductGroup": ', "Grain":', "BleedsTop": '0.0625',
+                         "BleedsLeft": '0.0625', "BleedsBottom": '0.0625',
+                         "BleedsRight": '0.0625',
+                         "FolioSide1": "Front", "FolioSide2": "Back"}
+
+    def merge_notes_without_csv(self):
+       if "notes" in self.notes:
+            self.row["Notes"] = self.notes["notes"]
+       self.row["ContentFile"] = self.pdf
+       if "group" in self.notes:
+            self.row["ProductGroup"] = self.notes["group"]
+       return self.row
+
+class Bound:
+    CSV_HEADERS_BOUND_SELF = ["comment", "Name", "Quantity", "Width", "Height",
+                              "StockVendor", "StockName", "StockWeight",
+                              "IGNORED0", "TextPageCount",
+                              "LargestTextComponent",
+                              "BindingMachine", "IGNORED1", "IGNORED2",
+                              "IGNORED3",
+                              "ProductID", "Description", "Notes", "DueDate",
+                              "CompanyName", "First -> objectName", "LastName",
+                              "ContentFile", "IGNORED4", "INGORED5",
+                              "PageColorName", "IGNORED6", "BleedsTop",
+                              "BleedsLeft", "BleedsBottom", "BleedsRight",
+                              "FolioPattern", "TextFolds", "IGNORED7",
+                              "BindingNumberUp", "1stUpOrientation",
+                              "NUpOrientation", "Grain"]
+
+    def __init__(self, pdf, notes):
+        self.pdf = pdf
+        self.notes = notes
+        self.row = {"comment": "MetrixCSV2.0_BOUND_SELF_COVER", "Name": pdf[:-4],
+                    "Quantity": notes["quantity"], "Width": notes["width"],
+                    "Height": notes["height"], "StockVendor": "PG",
+                    "StockName": notes["stockname"],
+                    "StockWeight": notes["stockweight"],
+                    "IGNORED0": "", "TextPageCount": "", "LargestTextComponent": "4",
+                    "BindingMachine": "DUPLO", "IGNORED1": "", "IGNORED2": "",
+                    "IGNORED3": "", "ProductID": "",
+                    "Description": pdf.split('-')[0], "Notes": "",
+                    "DueDate": "", "CompanyName": pdf.split('-')[2],
+                    "FirstName": "",
+                    "LastName": "", "ContentFile": pdf, "IGNORED4": "",
+                    "INGORED5": "",
+                    "PageColorName": "Cyan, Magenta, Yellow, Black",
+                    "IGNORED6": "",
+                    "BleedsTop": "0.0625", "BleedsLeft": "0.0625",
+                    "BleedsBottom": "0.0625",
+                    "BleedsRight": "0.0625", "FolioPattern": "", "TextFolds": "",
+                    "IGNORED7": "", "BindingNumberUp": "",
+                    "1stUpOrientation": "HeadToJog",
+                    "NUpOrientation": "", "Grain": ""}
+
+    def merge_notes_without_csv(self):
+        if "pages" in self.notes:
+            self.row["TextPageCount"] = self.notes["pages"]
+        if "group" in self.notes:
+            self.row["Notes"] = self.notes["group"]
+        if "notes" in self.notes:
+            self.row["Notes"] += " " + self.notes["notes"]
+
+        return self.row
