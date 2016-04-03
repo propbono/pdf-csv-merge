44214ac9f9772c935833fd4bf3c62796eefdba8b
Grzegorz Mozer <propbono@gmail.com>
2016-04-03 11:22:48 -0400

    Extract saving method to data class

---
+++ Data.py
@@ -0,0 +1,121 @@
+import csv
+import datetime
+import os
+
+import Product
+from Configuration import Configuration
+from Notes import Notes
+
+
+class ReturnData:
+    def __init__(self, processed = 0, skipped = 0, flat = {},
+                 bound = {}):
+        self.files_added_to_csv = processed
+        self.files_skipped = skipped
+        self.rows_flat = flat
+        self.rows_bound = bound
+
+
+class Data:
+
+
+    def __init__(self, pdf_list = None):
+        self.pdf_list = pdf_list
+        self.config = Configuration.factory()
+        self.CSV_HEADERS_FLAT = ["comment", "Name", "Quantity",
+                                         "Width", "Height",
+                                         "StockVendor", "StockName",
+                                         "StockWeight", "IGNORED1",
+                                         "IGNORED2", "Priority", "TopOffcut",
+                                         "LeftOffcut",
+                                         "BottomOffcut", "RightOffcut",
+                                         "ProductID",
+                                         "Description", "Notes", "DueDate",
+                                         "CompanyName",
+                                         "FirstName", "LastName",
+                                         "ContentFile", "PageColor1",
+                                         "PageColor2", "ProductGroup", "Grain",
+                                         "BleedsTop",
+                                         "BleedsLeft", "BleedsBottom",
+                                         "BleedsRight",
+                                         "FolioSide1", "FolioSide2"]
+
+        self.CSV_HEADERS_BOUND_SELF = ["comment", "Name", "Quantity", "Width",
+                                       "Height",
+                                       "StockVendor", "StockName",
+                                       "StockWeight",
+                                       "IGNORED0", "TextPageCount",
+                                       "LargestTextComponent",
+                                       "BindingMachine", "IGNORED1",
+                                       "IGNORED2", "IGNORED3",
+                                       "ProductID", "Description", "Notes",
+                                       "DueDate",
+                                       "CompanyName", "FirstName", "LastName",
+                                       "ContentFile", "IGNORED4", "INGORED5",
+                                       "PageColorName", "IGNORED6",
+                                       "BleedsTop",
+                                       "BleedsLeft", "BleedsBottom",
+                                       "BleedsRight",
+                                       "FolioPattern", "TextFolds", "IGNORED7",
+                                       "BindingNumberUp", "1stUpOrientation",
+                                       "NUpOrientation", "Grain"]
+
+
+    def __add_data_to_dict(self):
+        print("Collecting data to dictionary:")
+
+        data = ReturnData()
+        for pdf in self.pdf_list:
+            notes = Notes()
+            try:
+                pdf_without_notes, notes = notes.extract_notes_from(pdf)
+            except:
+                continue #check impact
+            else:
+                if notes == None:
+                    data.files_skipped += 1
+                    print("NOT PREPPED: ", pdf_without_notes)
+                else:
+                    product = Product.Product.factory(pdf_without_notes, notes)
+                    row = product.merge_notes_without_csv()
+
+                    key = notes["stock"]
+                    if notes["type"] == "FLAT":
+                        data.rows_flat.setdefault(key,[]).append(row)
+                    elif notes["type"] == "BOUND":
+                        data.rows_bound.setdefault(key, []).append(row)
+
+                    print("ADDED!!: ", pdf)
+                    data.files_added_to_csv += 1
+
+        print("All data in dictionary!")
+        return data
+
+
+    def __save_csv_dict_data(self,key, data_dict, headers):
+        today = datetime.datetime.today().strftime("%Y-%m-%d")
+        now = datetime.datetime.now().strftime("%Y-%m-%dT%H%M")  # date:
+        # 2015-11-03T1935
+        dir_name = os.path.join(self.config.MERGED_CSV_LOCAL, today)
+        csv_file_name = os.path.join(self.config.MERGED_CSV_LOCAL, dir_name,
+                                     key + '-' + now + '.csv')
+
+        if not os.path.isdir(dir_name):
+            os.mkdir(dir_name)
+        if not os.path.exists(csv_file_name):
+            with open(csv_file_name, 'a', newline = '') as csv_file:
+                writer = csv.DictWriter(csv_file, fieldnames = headers)
+                writer.writeheader()
+                writer.writerows(data_dict)
+
+
+    def save_all_csv(self):
+        dict_data = self.__add_data_to_dict()
+        for key in dict_data.rows_flat.keys():
+            self.__save_csv_dict_data(key, dict_data.rows_flat[key],
+                                  self.CSV_HEADERS_FLAT)
+        for key in dict_data.rows_bound.keys():
+            self.__save_csv_dict_data(key, dict_data.rows_bound[key],
+                                 self.CSV_HEADERS_BOUND_SELF)
+
+
--- Merge.py
+++ Merge.py
@@ -2,96 +2,33 @@
 
 # __author__ = 'propbono@gmail.com'
 import timeit
-import csv
 
-from DictionaryData import *
+from Data import *
 from Move import *
 
 
-CSV_HEADERS_FLAT = ["comment", "Name", "Quantity", "Width", "Height",
-                    "StockVendor", "StockName", "StockWeight", "IGNORED1",
-                    "IGNORED2", "Priority", "TopOffcut", "LeftOffcut",
-                    "BottomOffcut", "RightOffcut", "ProductID",
-                    "Description", "Notes", "DueDate", "CompanyName",
-                    "FirstName", "LastName", "ContentFile", "PageColor1",
-                    "PageColor2", "ProductGroup", "Grain", "BleedsTop",
-                    "BleedsLeft", "BleedsBottom", "BleedsRight",
-                    "FolioSide1", "FolioSide2"]
-
-CSV_HEADERS_BOUND_SELF = ["comment", "Name", "Quantity", "Width", "Height",
-                          "StockVendor", "StockName", "StockWeight",
-                          "IGNORED0", "TextPageCount", "LargestTextComponent",
-                          "BindingMachine", "IGNORED1", "IGNORED2", "IGNORED3",
-                          "ProductID", "Description", "Notes", "DueDate",
-                          "CompanyName", "FirstName", "LastName",
-                          "ContentFile", "IGNORED4", "INGORED5",
-                          "PageColorName", "IGNORED6", "BleedsTop",
-                          "BleedsLeft", "BleedsBottom", "BleedsRight",
-                          "FolioPattern", "TextFolds", "IGNORED7",
-                          "BindingNumberUp", "1stUpOrientation",
-                          "NUpOrientation", "Grain"]
-
 # in production add parameter "Working"
 config = Configuration.factory()
-clean = Move()
-dict_data = Data()
+cleaner = Move()
+dict_data = ReturnData()
 
-
-def __save_csv_dict_data(key, data_dict, headers):
-    today = datetime.datetime.today().strftime("%Y-%m-%d")
-    now = datetime.datetime.now().strftime("%Y-%m-%dT%H%M")  # date:
-    # 2015-11-03T1935
-    dir_name = os.path.join(config.MERGED_CSV_LOCAL, today)
-    csv_file_name = os.path.join(config.MERGED_CSV_LOCAL, dir_name,
-                                 key + '-' + now + '.csv')
-
-    if not os.path.isdir(dir_name):
-        os.mkdir(dir_name)
-    if not os.path.exists(csv_file_name):
-        with open(csv_file_name, 'a', newline = '') as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames = headers)
-            writer.writeheader()
-            writer.writerows(data_dict)
 
 def merge_csv_from(pdf_list):
-    print("Collecting data to dictionary:")
-    dict_tic = timeit.default_timer()
 
-    data = DictionaryData(pdf_list)
-    dict_data = data.add_data_to_dict()
-
-    dict_toc = timeit.default_timer()
-    print("All data in dictionary!", "time (s): ",
-          round(dict_toc - dict_tic, 4))
-
+    data = Data(pdf_list)
 
     print("Creating CSV's:")
-    csv_tic = timeit.default_timer()
-
-    for key in dict_data.rows_flat.keys():
-        __save_csv_dict_data(key, dict_data.rows_flat[key], CSV_HEADERS_FLAT)
-    for key in dict_data.rows_bound.keys():
-        __save_csv_dict_data(key, dict_data.rows_bound[key], CSV_HEADERS_BOUND_SELF)
-
-    csv_toc = timeit.default_timer()
-    print("CSV - created!", "time (s): ", round(csv_toc - csv_tic, 4))
-
+    data.save_all_csv()
+    print("CSV - created!")
 
     print("Copying CSV's:")
-    copy_csv_tic = timeit.default_timer()
+    cleaner.move_merged_csv()
+    print("CSV - copied!")
 
-    clean.move_merged_csv()
-
-    copy_csv_toc = timeit.default_timer()
-    print("CSV - copied!", "time (s): ", round(copy_csv_toc - copy_csv_tic, 4))
 
     print("Moving pdf's:")
-    move_pdf_tic = timeit.default_timer()
-
-    clean.rename_and_move_pdf(pdf_list)
-
-    move_pdf_toc = timeit.default_timer()
-    print("Pdf - moved!", "time (s): ", round(move_pdf_toc - move_pdf_tic, 4))
+    cleaner.rename_and_move_pdf(pdf_list)
+    print("Pdf - moved!")
 
     return dict_data.files_added_to_csv, dict_data.files_skipped
 
