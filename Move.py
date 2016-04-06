ac5c1692d53471900aa90319a2731a88bb10f8ef
Grzegorz Mozer <propbono@gmail.com>
2016-04-06 19:49:09 -0400

    pull up creating a press ready pdf list outside of the loop . we don't need to do that every time

--- Move.py
+++ Move.py
@@ -15,11 +15,12 @@ class Move(object):
 
 
     def rename_and_move_pdf(self, pdf_list):
+        press_ready_pdf_list = self.__press_ready_pdf_list()
         for i, pdf in enumerate(pdf_list, 1):
             tic = timeit.default_timer()
             notes = Notes()
             new_pdf = notes.delete_prepp_notes_from(pdf)
-            if new_pdf in self.__press_ready_pdf_list():
+            if new_pdf in press_ready_pdf_list:
                 self.pdf_to_delete.append(pdf)
                 toc = timeit.default_timer()
                 print(i, new_pdf[:7], "NOT MOVED, time: ", round(toc - tic, 4), "s")
